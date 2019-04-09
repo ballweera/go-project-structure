1147e7475c0eaa0575800f75f112545d1091efa4
Weera Kasetsin <wkasetsin@gmail.com>
2019-04-09 17:37:48 +0700

    Add NewProducer() and NewConsumer()

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -3,11 +3,19 @@ package kafka
 type Producer struct {
 }
 
+func NewProducer() Producer {
+	return Producer{}
+}
+
 func (p Producer) Produce(msg string) error {
 	return nil
 }
 
 type Consumer struct {
+}
+
+func NewConsumer() Consumer {
+	return Consumer{}
 }
 
 func (c Consumer) Consume() error {
